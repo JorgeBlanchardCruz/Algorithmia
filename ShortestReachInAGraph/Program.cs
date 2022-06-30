9e264255a4bee63f3e3bfba365fb2b09e510a0c0
Jorge Blanchard Cruz <rothgard@live.com>
2022-06-30 20:08:44 +0100

    IMPROV for a test

--- ShortestReachInAGraph/Program.cs
+++ ShortestReachInAGraph/Program.cs
@@ -1,40 +1,64 @@
-﻿using ShortestReachInAGraph;
+﻿global using System.Text;
+using ShortestReachInAGraph;
 
-var graphs = new List<string>() {
-    "2",
-    "4 2",
-    "1 2",
-    "1 3",
-    "1",
-    "3 1",
-    "2 3",
-    "2"
-};
+//var graphs = new List<string>() {
+//    "2",
+//    "4 2",
+//    "1 2",
+//    "1 3",
+//    "1",
+//    "3 1",
+//    "2 3",
+//    "2"
+//};
 
-var expected = new List<string>() { 
-    "6 6 -1 ",
-    "-1 6"
-};
+//var expected = new List<string>() { 
+//    "6 6 -1 ",
+//    "-1 6"
+//};
 
-var result = ProblemResolution.ProcessGraphs(graphs);
-
-Console.WriteLine($"expected");
-expected.ForEach(Console.WriteLine);
-
-Console.WriteLine($"{Environment.NewLine}result");
-result.ForEach(Console.WriteLine);
-
-Console.ReadKey();
-
-
-
-int problemCount = Convert.ToInt32(Console.ReadKey());
-int currentProblem = 1;
-do
+class Solution
 {
+    static void Main(String[] args)
+    {
+        var graphs = new List<string>() {
+            "7 4",
+            "1 2",
+            "1 3",
+            "3 4",
+            "2 5",
+            "2"
+        };
+
+        var results = ProblemResolution.ProcessGraphs(graphs);
+        Console.WriteLine(results);
+    }
+}
 
 
+//class Solution
+//{
+//    static void Main(String[] args)
+//    {
+//        List<string> results = new();
 
-} while ((currentProblem <= problemCount));
+//        int problemCount = int.Parse(Console.ReadLine());
+//        int currentProblem = 1;
+//        while (currentProblem <= problemCount)
+//        {
+//            List<string> problem = new();
+//            string lineInput = string.Empty;
+//            do
+//            {
+//                lineInput = Console.ReadLine();
+//                problem.Add(lineInput);
+//            }
+//            while (lineInput != string.Empty && lineInput.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).Length == 2);
 
+//            results.Add(ProblemResolution.ProcessGraphs(problem));
+//            currentProblem++;
+//        }
 
+//        results.ForEach(Console.WriteLine);
+//    }
+//}
